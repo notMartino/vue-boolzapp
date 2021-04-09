4f544e86f75e614ae2b6b5fc3ac4628d5e60fb08
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-04-09 23:55:10 +0200

    Milestone2-3: visual. dinamica msg, click contatto, invio msg e risposta

--- js/script.js
+++ js/script.js
@@ -2,7 +2,13 @@ function initVue() {
     new Vue({
         el: '#vue',
         data: {
-            lol: 'ciao',
+            activated: '',
+            name: '',
+            src: '',
+            sent: '',
+            msg: '',
+            user: '',
+            enterMsg: '',
             contacts: [
                 {
                     name: 'Alessia Lee',
@@ -10,17 +16,20 @@ function initVue() {
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/21',
+                            hour: '15:30:00',
                             text: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/21',
+                            hour: '15:50:00',
                             text: 'Ricordati di dargli da mangiare',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 16:15:22',
+                            date: '10/01/21', 
+                            hour: '16:15:00',
                             text: "Mi manchi!",
                             status: 'received'
                         }
@@ -32,17 +41,20 @@ function initVue() {
                     visible: true,
                     messages: [
                         {
-                            date: '20/03/2020 16:30:00',
+                            date: '20/03/21',
+                            hour: '16:30:00',
                             text: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
-                            date: '20/03/2020 16:30:55',
+                            date: '20/03/21',
+                            hour: '16:30:55',
                             text: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
-                            date: '20/03/2020 16:35:00',
+                            date: '20/03/21',
+                            hour: '16:35:00',
                             text: 'Mi piacerebbe ma devo andare a fare la spesa.',
                             status: 'sent'
                         }
@@ -54,17 +66,20 @@ function initVue() {
                     visible: true,
                     messages: [
                         {
-                            date: '28/03/2020 10:10:40',
+                            date: '28/03/21',
+                            hour: '10:10:40',
                             text: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
-                            date: '28/03/2020 10:20:10',
+                            date: '28/03/21',
+                            hour: '10:20:10',
                             text: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
-                            date: '28/03/2020 16:15:22',
+                            date: '28/03/21',
+                            hour: '16:15:22',
                             text: 'Ah scusa!',
                             status: 'received'
                         }
@@ -76,12 +91,14 @@ function initVue() {
                     visible: true,
                     messages: [
                         {
-                            date: '10/01/2020 15:30:55',
+                            date: '10/01/21',
+                            hour: '15:30:55',
                             text: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
-                            date: '10/01/2020 15:50:00',
+                            date: '10/01/21',
+                            hour: '15:50:00',
                             text: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
@@ -89,9 +106,61 @@ function initVue() {
                 },
             ]
         },
-        mounted() {
-            console.log(this.contacts[1].imgSrc);
-        }
+        methods:{
+            selectedChat: function (contact) {
+                this.name = contact.name;
+                this.activated = 'active';
+                this.src = contact.imgSrc;
+                
+                this.msg = contact.messages;
+                console.log(this.msg);
+                this.user = contact;
+            },
+            answer: function () {
+                console.log('ciaoaaaa');
+                let moment = new Date();
+                let date = `${moment.getDate()}/${(moment.getMonth() + 1)}/${moment.getFullYear().toString().slice(2)}`;  
+                let hours = `${moment.getHours()}:${moment.getMinutes()}:${moment.getSeconds()}`;
+
+                let text = 'Va bene!';
+                let status = 'received';
+
+                this.user.messages.push({date, hours, text, status});
+                console.log(this.user.messages);
+            },
+            enterMsgs: function () {
+                let moment = new Date();
+                let date = `${moment.getDate()}/${(moment.getMonth() + 1)}/${moment.getFullYear().toString().slice(2)}`;  
+                let hours = `${moment.getHours()}:${moment.getMinutes()}:${moment.getSeconds()}`;
+
+                let text = this.enterMsg;
+                let status = 'sent';
+
+                if(this.enterMsg.length > 0 && this.enterMsg[0] != ' '){
+                    this.user.messages.push({date, hours, text, status});
+                    console.log(this.user.messages);
+                    this.enterMsg = '';
+                    setTimeout(this.answer, 2000);
+                }
+            }
+        },
+        // mounted() {
+        //     function orderBydate(contacts) {
+        //         let lastContact = 0;
+        //         let thisContact;
+        //         for (let i = 0; i < contacts.length; i++) {
+        //             // thisContact = contacts[i];
+        //             const dateMsg = contacts[i].messages[contacts[i].messages.length - 1].date;
+        //             console.log(dateMsg);
+        //             let hour = dateMsg.slice(0,2);
+        //             console.log(hour);
+        //             if(lastContact == 0){
+        //                 lastContact = contacts[i];
+        //             }
+        //         }
+        //     }
+        //     orderBydate(this.contacts);
+        // }
     });
 }
 
