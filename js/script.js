6e05b2fb1c8ae3f5f1cc0904390256544405293a
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-04-09 16:25:46 +0200

    Milestone1 completa: lista chat contatti dinamica con utilizzo di v-for

--- js/script.js
+++ js/script.js
@@ -1,1 +1,102 @@
-console.log('CIAO');
+function initVue() {
+    new Vue({
+        el: '#vue',
+        data: {
+            lol: 'ciao',
+            contacts: [
+                {
+                    name: 'Alessia Lee',
+                    imgSrc: 'img/ale.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Ricordati di dargli da mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            text: "Mi manchi!",
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Trecca',
+                    imgSrc: 'img/trecca.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            text: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            text: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'More',
+                    imgSrc: 'img/more.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            text: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            text: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            text: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Alessia Pellegrini',
+                    imgSrc: 'img/alessiadonb.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            text: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            text: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+            ]
+        },
+        mounted() {
+            console.log(this.contacts[1].imgSrc);
+        }
+    });
+}
+
+function init(){
+    initVue();
+}
+
+document.addEventListener('DOMContentLoaded', init);
