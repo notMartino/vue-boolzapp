385db9b7e85ecb96bdd81a4a49ba8c4dc652d9aa
Martino Scalvini <martinoscalvinitobia@gmail.com>
2021-04-11 12:51:10 +0200

    Milestone4-5: ricerca user, rimozione msg, ultimo msg e ora in lista
    Milestone 4: ricerca chat per per nome utente
    Milestone 5: 
    - rimozione messaggio in chat
    - rimozione chat in lista quando viene rimosso l'ultimo messaggio
    - visualizzazione ultimo messaggio e orario ricezione in lista chat (formato standardizzato ora:min/00:00)

--- js/script.js
+++ js/script.js
@@ -2,13 +2,16 @@ function initVue() {
     new Vue({
         el: '#vue',
         data: {
-            activated: '',
-            name: '',
-            src: '',
-            sent: '',
-            msg: '',
-            user: '',
-            enterMsg: '',
+            activated: '', // Classe active main attivato
+            name: '', // Nome contatto selected
+            src: '', // Src immagine contatto selected
+            sent: '', // Classe sent per chat verde
+            msg: '', // Array msg utente selezionto
+            user: '', // Utente selzionato
+            enterMsg: '', // Messaggio inviato da input
+            search: '', // Nome da cercare via input
+            deletedActive: '', // Classe active delete attivato
+            indexUser: -1, // Indice in contacs dell'utente selected
             contacts: [
                 {
                     name: 'Alessia Lee',
@@ -16,18 +19,21 @@ function initVue() {
                     visible: true,
                     messages: [
                         {
+                            delete: false,
                             date: '10/01/21',
                             hour: '15:30:00',
                             text: 'Hai portato a spasso il cane?',
                             status: 'sent'
                         },
                         {
+                            delete: false,
                             date: '10/01/21',
                             hour: '15:50:00',
                             text: 'Ricordati di dargli da mangiare',
                             status: 'sent'
                         },
                         {
+                            delete: false,
                             date: '10/01/21', 
                             hour: '16:15:00',
                             text: "Mi manchi!",
@@ -41,18 +47,21 @@ function initVue() {
                     visible: true,
                     messages: [
                         {
+                            delete: false,
                             date: '20/03/21',
                             hour: '16:30:00',
                             text: 'Ciao come stai?',
                             status: 'sent'
                         },
                         {
+                            delete: false,
                             date: '20/03/21',
                             hour: '16:30:55',
                             text: 'Bene grazie! Stasera ci vediamo?',
                             status: 'received'
                         },
                         {
+                            delete: false,
                             date: '20/03/21',
                             hour: '16:35:00',
                             text: 'Mi piacerebbe ma devo andare a fare la spesa.',
@@ -66,18 +75,21 @@ function initVue() {
                     visible: true,
                     messages: [
                         {
+                            delete: false,
                             date: '28/03/21',
                             hour: '10:10:40',
                             text: 'La Marianna va in campagna',
                             status: 'received'
                         },
                         {
+                            delete: false,
                             date: '28/03/21',
                             hour: '10:20:10',
                             text: 'Sicuro di non aver sbagliato chat?',
                             status: 'sent'
                         },
                         {
+                            delete: false,
                             date: '28/03/21',
                             hour: '16:15:22',
                             text: 'Ah scusa!',
@@ -91,57 +103,100 @@ function initVue() {
                     visible: true,
                     messages: [
                         {
+                            delete: false,
                             date: '10/01/21',
                             hour: '15:30:55',
                             text: 'Lo sai che ha aperto una nuova pizzeria?',
                             status: 'sent'
                         },
                         {
+                            delete: false,
                             date: '10/01/21',
                             hour: '15:50:00',
                             text: 'Si, ma preferirei andare al cinema',
                             status: 'received'
                         }
                     ],
-                },
+                }
             ]
         },
         methods:{
-            selectedChat: function (contact) {
+            // Funzione filtro per nome cercato
+            research: function () {
+                let filteredContacts = [];
+                let searched = this.search.toLowerCase();
+
+                for (let i = 0; i < this.contacts.length; i++) {
+                    // if(this.search == ''){
+                    //     filteredContacts = this.contacts;
+                    if(this.contacts[i].name.toLowerCase().includes(searched)){
+                        filteredContacts.push(this.contacts[i]);
+                    }
+                }
+                return filteredContacts;
+            },
+            // Funzione chat selezionata
+            selectedChat: function (contact, index) {
                 this.name = contact.name;
                 this.activated = 'active';
                 this.src = contact.imgSrc;
-                
                 this.msg = contact.messages;
-                console.log(this.msg);
-                this.user = contact;
+                this.user = contact; 
+                this.indexUser = index;
             },
+            // Funzione di risposta automatica
             answer: function () {
-                console.log('ciaoaaaa');
                 let moment = new Date();
                 let date = `${moment.getDate()}/${(moment.getMonth() + 1)}/${moment.getFullYear().toString().slice(2)}`;  
-                let hours = `${moment.getHours()}:${moment.getMinutes()}:${moment.getSeconds()}`;
+                let hour = `${this.formatDate(moment.getHours())}:${this.formatDate(moment.getMinutes())}:${this.formatDate(moment.getSeconds())}`;
 
                 let text = 'Va bene!';
                 let status = 'received';
 
-                this.user.messages.push({date, hours, text, status});
+                this.user.messages.push({date, hour, text, status});
                 console.log(this.user.messages);
             },
+            // Funzione invio messaggio
             enterMsgs: function () {
                 let moment = new Date();
                 let date = `${moment.getDate()}/${(moment.getMonth() + 1)}/${moment.getFullYear().toString().slice(2)}`;  
-                let hours = `${moment.getHours()}:${moment.getMinutes()}:${moment.getSeconds()}`;
+                let hour = `${this.formatDate(moment.getHours())}:${this.formatDate(moment.getMinutes())}:${this.formatDate(moment.getSeconds())}`;
 
                 let text = this.enterMsg;
                 let status = 'sent';
 
                 if(this.enterMsg.length > 0 && this.enterMsg[0] != ' '){
-                    this.user.messages.push({date, hours, text, status});
+                    this.user.messages.push({date, hour, text, status});
                     console.log(this.user.messages);
                     this.enterMsg = '';
                     setTimeout(this.answer, 2000);
                 }
+            },
+            // Finestra rimuovi messagio compare
+            deleteMsg: function (ms) {
+                ms.delete = true;
+            },
+            // Finestra rimuovi messagio scompare
+            renewedMsg: function (ms) {
+                ms.delete = false;
+            },
+            // Rimozione messaggio
+            deleteThisMsg: function (index) {
+                
+                this.msg.splice(index, 1);
+                if(this.msg.length == 0){
+                    this.contacts.splice(this.indexUser, 1);
+                }
+                console.log('Elem arr msg: ' + this.msg.length);
+                console.log(this.indexUser);
+                console.log(this.msg);
+            },
+            // Formattazione data/ora
+            formatDate: function (format){
+                if (format < 10) {
+                    format = '0' + format;
+                }
+                return format;
             }
         },
         // mounted() {
